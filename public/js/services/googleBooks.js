beab353de85cdd934897d5dae181f307f76b340a
Mickaël Andrieu <andrieu.travail@gmail.com>
2017-12-03 14:56:32 +0100

    fixed call to GoogleBook api

--- public/js/services/googleBooks.js
+++ public/js/services/googleBooks.js
@@ -7,24 +7,11 @@ angular
 
     this.get = (query) => {
       let defer = $q.defer();
-      
-      $http.get('/googleBooks').then((response) => {
+  
+      return $http.get('/googleBooks/search/'+query).then((response) => {
         defer.resolve(response.data);
       }).catch((error) => {
         defer.reject(error);
       })
     }
-    /* this.get = (book) => {
-      
-      let defer = $q.defer();
-
-      $http.get(`https://www.googleapis.com/books/v1/volumes?q=${book}`).then((response) => {
-        defer.resolve(response.data);
-      }).catch((error) => {
-        console.log(error);
-        defer.reject(message);
-      });
-      return defer.promise;
-    }; */
-
   });
--- server/controllers/googleBooks.js
+++ server/controllers/googleBooks.js
@@ -1,25 +1,24 @@
+const https = require('https');
 const books = require('google-books-search');
-/* const apiUrl = "https://www.googleapis.com/books/v1/volumes?q="; */
+const apiUrl = "https://www.googleapis.com/books/v1/volumes?q=";
 /**
  * The Book controller object
  */
 function googleBooksController() {
-  let options = {
-    key: "AIzaSyATJN6kGNOxlU06GL27wQwoMQICvWcXTuc",
-    offset: 0,
-    limit: 10,
-    type: 'books',
-    order: 'relevance',
-    lang: ''
-  };
-
   googleBooksController.prototype.search = function (req, res, next) {
-    books.search(req.params, options, function (error, results, apiResponse) {
-      if (!error) {
-        console.log(results);
-      } else {
-        console.log(error);
-      }
+    https.get(apiUrl+req.params.query, (resp) => {
+      let data = '';
+    
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+    
+      resp.on('end', () => {
+        res.json(data);
+      });
+    
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
     });
   }
 
